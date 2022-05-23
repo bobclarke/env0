960373c4e44a55f4c76ab9eb93f2994f2ca4b7c8
Bob Clarke <bobmclarke@gmail.com>
2022-05-23 17:42:41 +0100

    update module ref

--- vmstack-tpl/main.tf
+++ vmstack-tpl/main.tf
@@ -1,4 +1,5 @@
+
 module "azurerm-vm-mod" {
-    source = "api.env0.com/ebb61ac8-d725-4e9d-8f5f-224074058393/azurerm-vm-mod/bobclarke"
-    version = "0.0.1"
+  source = "api.env0.com/58176a35-ba1f-4356-a7cd-338ac963391b/azurerm-vm-mod/bobclarke"
+  version = "0.0.2"
 }
